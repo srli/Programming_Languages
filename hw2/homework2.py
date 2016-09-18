fdae59aa5ade2e3a32b65ebb4181b90ecf6b1fc2
srli <c>
2016-09-18 15:24:18 -0400

    homework 2 completed

--- hw2/homework2.py
+++ hw2/homework2.py
@@ -28,7 +28,7 @@ class EInteger (Exp):
     def __str__ (self):
         return "EInteger({})".format(self._integer)
 
-    def eval (self,prim_dict):
+    def eval (self,prim_dict,func_dict):
         return VInteger(self._integer)
 
     def substitute (self,ids,new_es):
@@ -44,7 +44,7 @@ class EBoolean (Exp):
     def __str__ (self):
         return "EBoolean({})".format(self._boolean)
 
-    def eval (self,prim_dict):
+    def eval (self,prim_dict,func_dict):
         return VBoolean(self._boolean)
 
     def substitute (self,ids,new_es):
@@ -60,14 +60,35 @@ class EPrimCall (Exp):
     def __str__ (self):
         return "EPrimCall({},[{}])".format(self._name,",".join([ str(e) for e in self._exps]))
 
-    def eval (self,prim_dict):
-        vs = [ e.eval(prim_dict) for e in self._exps ]
+    def eval (self,prim_dict,func_dict):
+        vs = [ e.eval(prim_dict,func_dict) for e in self._exps ]
         return apply(prim_dict[self._name],vs)
 
     def substitute (self,ids,new_es):
         new_es = [ e.substitute(ids,new_es) for e in self._exps]
         return EPrimCall(self._name,new_es)
 
+class ECall (Exp):
+
+    def __init__(self, name, param_values):
+        self._name = name
+        self._param_values = param_values
+
+    def __str__(self):
+        return "ECall({},[{}])".format(self._name,",".join([ str(e) for e in self._param_values]))
+
+    def eval(self, prim_dict, func_dict):
+            params = func_dict.get(self._name).get("params")
+            if len(params) != len(self._param_values):
+                raise Exception("Runtime error: incorrect number of parameters given")
+            assignments = [(params[i], self._param_values[i]) for i in range(len(params))]
+            function_binding = ELet(assignments, func_dict.get(self._name).get("body"))
+
+            return function_binding.eval(prim_dict, func_dict)
+
+    def substitute (self,ids,new_es):
+        new_es = [ e.substitute(ids,new_es) for e in self._param_values]
+        return ECall(self._name,new_es)
 
 class EIf (Exp):
     # Conditional expression
@@ -80,14 +101,14 @@ class EIf (Exp):
     def __str__ (self):
         return "EIf({},{},{})".format(self._cond,self._then,self._else)
 
-    def eval (self,prim_dict):
-        v = self._cond.eval(prim_dict)
+    def eval (self,prim_dict,func_dict):
+        v = self._cond.eval(prim_dict,func_dict)
         if v.type != "boolean":
             raise Exception ("Runtime error: condition not a Boolean")
         if v.value:
-            return self._then.eval(prim_dict)
+            return self._then.eval(prim_dict,func_dict)
         else:
-            return self._else.eval(prim_dict)
+            return self._else.eval(prim_dict,func_dict)
 
     def substitute (self,ids,new_es):
         return EIf(self._cond.substitute(ids,new_es),
@@ -107,12 +128,12 @@ class ELet (Exp):
         # TODO: update this
         return "ELet({},{},{})".format(self._id,self._e1,self._e2)
 
-    def eval (self,prim_dict):
+    def eval (self,prim_dict,func_dict):
         # print(self._e2.__str__())
 
         new_e2 = self._e2.substitute(self._ids, self._e1s)
         # print([x.value for x in self._e1s])
-        return new_e2.eval(prim_dict)
+        return new_e2.eval(prim_dict,func_dict)
 
     def substitute (self,ids, new_e1s):
         new_assignments = []
@@ -121,7 +142,7 @@ class ELet (Exp):
 
         # goes through all of the assignments
         for j in range(len(self._ids)):
-            # substitutes values from upper assignment as needed 
+            # substitutes values from upper assignment as needed
             for i in range(len(ids)):
                 id = ids[i]
                 new_e1 = new_e1s[i]
@@ -133,6 +154,34 @@ class ELet (Exp):
         return ELet(new_assignments, new_e2)
 
 
+
+class ELetV (Exp):
+    # local binding
+
+    def __init__ (self,id,e1,e2):
+        self._id = id
+        self._e1 = e1
+        self._e2 = e2
+
+    def __str__ (self):
+        return "ELet({},{},{})".format(self._id,self._e1,self._e2)
+
+    def eval (self,prim_dict,func_dict):
+        new_e = EInteger(self._e1.eval(prim_dict,func_dict).value)
+        new_e2 = self._e2.substitute(self._id,new_e)
+        return new_e2.eval(prim_dict,func_dict)
+
+    def substitute (self,id,new_e):
+        if id == self._id:
+            return ELetV(self._id,
+                        self._e1.substitute(id,new_e),
+                        self._e2)
+        return ELetV(self._id,
+                    self._e1.substitute(id,new_e),
+                    self._e2.substitute(id,new_e))
+
+
+
 class EId (Exp):
     # identifier
 
@@ -142,19 +191,23 @@ class EId (Exp):
     def __str__ (self):
         return "EId({})".format(self._id)
 
-    def eval (self,prim_dict):
+    def eval (self,prim_dict,func_dict):
         raise Exception("Runtime error: unknown identifier {}".format(self._id))
 
     def substitute (self,ids,new_es):
-        for i in range(len(ids)):
-            id = ids[i]
-            new_e = new_es[i]
-            if id == self._id:
-                return new_e
+        if type(new_es) == list:
+            for i in range(len(ids)):
+                id = ids[i]
+                new_e = new_es[i]
+                if id == self._id:
+                    return new_e
+        else:
+            if ids == self._id:
+                return new_es
         return self
 
 
-    
+
 #
 # Values
 #
@@ -181,7 +234,7 @@ class VBoolean (Value):
 
 # Primitive operations
 
-def oper_plus (v1,v2): 
+def oper_plus (v1,v2):
     if v1.type == "integer" and v2.type == "integer":
         return VInteger(v1.value + v2.value)
     raise Exception ("Runtime error: trying to add non-numbers")
@@ -196,11 +249,30 @@ def oper_times (v1,v2):
         return VInteger(v1.value * v2.value)
     raise Exception ("Runtime error: trying to add non-numbers")
 
+def oper_zero (v1):
+    if v1.type == "integer":
+        return VBoolean(v1.value==0)
+    raise Exception ("Runtime error: type error in zero?")
+
 
 # Initial primitives dictionary
 
 INITIAL_PRIM_DICT = {
     "+": oper_plus,
     "*": oper_times,
-    "-": oper_minus
+    "-": oper_minus,
+    "zero?": oper_zero
 }
+
+FUN_DICT = {
+      "square": {"params":["x"],
+                 "body":EPrimCall("*",[EId("x"),EId("x")])},
+      "=": {"params":["x","y"],
+            "body":EPrimCall("zero?",[EPrimCall("-",[EId("x"),EId("y")])])},
+      "+1": {"params":["x"],
+             "body":EPrimCall("+",[EId("x"),EInteger(1)])},
+      "sum_from_to": {"params":["s","e"],
+                      "body":EIf(ECall("=",[EId("s"),EId("e")]),
+                                EId("s"),EPrimCall("+",[EId("s"),
+                                ECall("sum_from_to",[ECall("+1",[EId("s")]), EId("e")])]))}
+    }
--- hw2/testbench.py
+++ hw2/testbench.py
@@ -4,12 +4,37 @@ import unittest
 class TestCases(unittest.TestCase):
 
     def test_one(self):
-        self.assertEqual(ELet([("a",EInteger(99))],EId("a")).eval(INITIAL_PRIM_DICT).value,  99)
-        self.assertEqual(ELet([("a",EInteger(99)),("b",EInteger(66))],EId("a")).eval(INITIAL_PRIM_DICT).value, 99)
-        self.assertEqual(ELet([("a",EInteger(99)),("b",EInteger(66))],EId("b")).eval(INITIAL_PRIM_DICT).value, 66)
-        self.assertEqual(ELet([("a",EInteger(99))],ELet([("a",EInteger(66)),("b",EId("a"))],EId("a"))).eval(INITIAL_PRIM_DICT).value, 66)
-        self.assertEqual(ELet([("a",EInteger(99))],ELet([("a",EInteger(66)),("b",EId("a"))],EId("b"))).eval(INITIAL_PRIM_DICT).value, 99)
-        self.assertEqual(ELet([("a",EInteger(5)),("b",EInteger(20))],ELet([("a",EId("b")),("b",EId("a"))],EPrimCall("-",[EId("a"),EId("b")]))).eval(INITIAL_PRIM_DICT).value,15)
+        self.assertEqual(ELet([("a",EInteger(99))],EId("a")).eval(INITIAL_PRIM_DICT, FUN_DICT).value,  99)
+        self.assertEqual(ELet([("a",EInteger(99)),("b",EInteger(66))],EId("a")).eval(INITIAL_PRIM_DICT, FUN_DICT).value, 99)
+        self.assertEqual(ELet([("a",EInteger(99)),("b",EInteger(66))],EId("b")).eval(INITIAL_PRIM_DICT, FUN_DICT).value, 66)
+        self.assertEqual(ELet([("a",EInteger(99))],ELet([("a",EInteger(66)),("b",EId("a"))],EId("a"))).eval(INITIAL_PRIM_DICT, FUN_DICT).value, 66)
+        self.assertEqual(ELet([("a",EInteger(99))],ELet([("a",EInteger(66)),("b",EId("a"))],EId("b"))).eval(INITIAL_PRIM_DICT, FUN_DICT).value, 99)
+        self.assertEqual(ELet([("a",EInteger(5)),("b",EInteger(20))],ELet([("a",EId("b")),("b",EId("a"))],EPrimCall("-",[EId("a"),EId("b")]))).eval(INITIAL_PRIM_DICT, FUN_DICT).value,15)
+
+    def test_two(self):
+        self.assertEqual(ELetV("a",EInteger(10),EId("a")).eval(INITIAL_PRIM_DICT, FUN_DICT).value, 10)
+        self.assertEqual(ELetV("a",EInteger(10),ELetV("b",EInteger(20),EId("a"))).eval(INITIAL_PRIM_DICT, FUN_DICT).value, 10)
+        self.assertEqual(ELetV("a",EInteger(10),ELetV("a",EInteger(20),EId("a"))).eval(INITIAL_PRIM_DICT, FUN_DICT).value, 20)
+        self.assertEqual(ELetV("a",EPrimCall("+",[EInteger(10),EInteger(20)]), ELetV("b",EInteger(20),EId("a"))).eval(INITIAL_PRIM_DICT, FUN_DICT).value, 30)
+        self.assertEqual(ELetV("a",EPrimCall("+",[EInteger(10),EInteger(20)]), ELetV("b",EInteger(20),
+        EPrimCall("*",[EId("a"),EId("a")]))).eval(INITIAL_PRIM_DICT, FUN_DICT).value, 900)
+
+    def test_three(self):
+        self.assertEqual(ECall("+1",
+          [EInteger(100)]).eval(INITIAL_PRIM_DICT,FUN_DICT).value, 101)
+
+        self.assertEqual(ECall("+1",
+          [EPrimCall("+",[EInteger(100),EInteger(200)])]).eval(INITIAL_PRIM_DICT,FUN_DICT).value, 301)
+
+        self.assertEqual(ECall("+1",
+          [ECall("+1",
+                 [EInteger(100)])]).eval(INITIAL_PRIM_DICT,FUN_DICT).value, 102)
+
+        self.assertEqual(ECall("=",[EInteger(1),EInteger(2)]).eval(INITIAL_PRIM_DICT,FUN_DICT).value, False)
+
+        self.assertEqual(ECall("=",[EInteger(1),EInteger(1)]).eval(INITIAL_PRIM_DICT,FUN_DICT).value, True)
+
+        self.assertEqual(ECall("sum_from_to",[EInteger(0),EInteger(10)]).eval(INITIAL_PRIM_DICT,FUN_DICT).value, 55)
 
 if __name__ == '__main__':
     unittest.main()
