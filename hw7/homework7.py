4d74d9ec250b61492a21719fcd8705222b6370e4
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2016-11-16 00:23:16 -0500

    trying to get let to be a thing

--- hw7/homework7.py
+++ hw7/homework7.py
@@ -671,13 +671,14 @@ def callProcedure(result):
 
 def parsePLet (input):
     print "INPUT: ", input
-    bindings = input[3]
+    bindings = input[2]
     names = []
     expressions = []
     for binding in bindings:
+        print("BINDING: ", binding)
         names.append(binding[0])
         expressions.append(binding[1])
-    function = input[5]
+    function = input[4]
     print "OUT: ", ECall(EFunction(names, function), [expressions])
     return ECall(EFunction(names, function), [expressions])
 
@@ -688,35 +689,40 @@ def pLET_exps_unpack_nat(result):
     while result[i] != ')':
         exps.append(result[i])
         i += 1
-    print(ELet(exps, result[i+1]))
+    print("EXPS: ", exps)
+    print("RETURNING: ", ELet(exps, result[i+1]).__str__())
     return ELet(exps,result[i+1])
 
 def parse_imp (input):
     # parse a string into an element of the abstract representation
 
     # Grammar:
-    #
-    # <expr> ::= <integer>
-    #            true
-    #            false
-    #            <identifier>
-    #            ( if <expr> <expr> <expr> )
-    #            ( function ( <name ... ) <expr> )
-    #            ( <expr> <expr> ... )
-    #
-    # <decl> ::= var name = expr ;
-    #
-    # <stmt> ::= if <expr> <stmt> else <stmt>
-    #            while <expr> <stmt>
-    #            name <- <expr> ;
-    #            print <expr> ;
-    #            <block>
-    #
-    # <block> ::= { <decl> ... <stmt> ... }
-    #
-    # <toplevel> ::= <decl>
-    #                <stmt>
-    #
+    # expr ::= integer literal                 # of the form 123 or -456
+    #    boolean literal                       # true , false
+    #    string literal                        # of the form "xyz"
+    #    id                                    # starts with a letter or _
+    #    expr + expr                           # adds integers / concatenates arrays / concatenates strings
+    #    expr * expr
+    #    expr - expr
+    #    expr == expr                          # equality (all types)
+    #    expr > expr                           # for integers and strings (lexicographic order)
+    #    expr >= expr                          # for integers and strings (lexicographic order)
+    #    expr < expr                           # for integers and strings (lexicographic order)
+    #    expr <= expr                          # for integers and strings (lexicographic order)
+    #    expr <> expr                          # this is "not equal" (all types)
+    #    expr and expr                         # short-circuiting
+    #    expr or expr                          # short-circuiting
+    #    not expr                                     
+    #    let ( id = expr , ... ) expr          # local binding
+    #    expr ? expr : expr                    # conditional
+    #    expr ( expr , ... )                   # function call
+    #    ( expr )
+    #    [ expr , ... ]                        # creates an array
+    #    fun ( id , ... ) body                 # anonymous function
+    #    fun id ( id , ... ) body              # recursive anonymous function
+    #    { id : expr , ... }                   # dictionary (record)
+    #    expr [ expr ]                         # array or string (a[2]) or dictionary (a["x"]) indexing  
+
 
 
     idChars = alphas+"_+*-?!=<>"
@@ -753,7 +759,7 @@ def parse_imp (input):
     pMULT_BINDING.setParseAction(lambda result: [(r[0],r[1]) for r in result])
 
     pLET = Keyword("let") + "(" + pMULT_BINDING + ")" + pEXPR
-    pLET.setParseAction(lambda result:pLET_exps_unpack_nat(result))
+    pLET.setParseAction(lambda result: pLET_exps_unpack_nat(result))
 
     pEXPRS = ZeroOrMore(pEXPR)
     pEXPRS.setParseAction(lambda result: [result])
